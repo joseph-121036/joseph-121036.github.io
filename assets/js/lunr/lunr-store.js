4ecf071dc7d289622be4bd22aee97a7c49b1dc06
joseph-121036 <a30708592@gmail.com>
2024-07-13 16:15:36 +0900

    commit1

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "이것은 테스트 입니까?",
+        "excerpt":"[공지사항1] 지킬블로그 신규 업데이트 안내 드립니다. [공지사항2] 지킬블로그 신규 업데이트 안내 드립니다. 버튼입니당 테테테테테 이미지 세부목차 이미지 세부목차찐 이미지 세부목차1-1과 2분의 1 승강장 이미지 세부목차2 이미지 세부목차3 Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod....","categories": ["RTOS"],
+        "tags": ["python","blog"],
+        "url": "/rtos/test1/",
+        "teaser": null
+      },{
+        "title": "이것은 테스트 입니다",
+        "excerpt":"testtest  Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.   ","categories": ["embedded"],
+        "tags": [],
+        "url": "/embedded/test/",
+        "teaser": null
+      }]
